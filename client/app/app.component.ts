e31ee27d6e0d9fd68905374cf45eeb2f3be4263c
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-08 18:01:34 -0700

    pass compliling

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {TaskService} from './services/task.service'
+import { TaskService } from './services/task.service'
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: "app.component.html",
-  providers:[TaskService]
+  providers: [TaskService]
 })
-export class AppComponent {}
+export class AppComponent { }
 
--- client/app/components/tasks/tasks.component.ts
+++ client/app/components/tasks/tasks.component.ts
@@ -22,21 +22,19 @@ export class TasksComponent {
             title: this.title,
             isDone: false
         }
-        this.taskService.addTask(newTask).subscribe(task=>{
+        this.taskService.addTask(newTask).subscribe(task => {
             this.tasks.push(task);
-            this.title='';
+            this.title = '';
         });
     }
+
     deleteTask(id) {
         var tasks = this.tasks;
 
-        this.taskService.deleteTask(id).subscribe(data=>{
-            if(data.n ==1)
-            {
-                for(var i =0; i < tasks.length; i++)
-                {
-                    if(tasks[i]._id ==id)
-                    {
+        this.taskService.deleteTask(id).subscribe(data => {
+            if (data.n == 1) {
+                for (var i = 0; i < tasks.length; i++) {
+                    if (tasks[i]._id == id) {
                         tasks.splice(i, 1);
                     }
                 }
@@ -50,7 +48,7 @@ export class TasksComponent {
             isDone: !task.isDone
         }
 
-        this.taskService.updateStatus(_task).subscribe(data=>{
+        this.taskService.updateStatus(_task).subscribe(data => {
             task.isDone = !task.isDone;
         });
     }
